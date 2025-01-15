3afcfe8952ff8bb813574ceccdeeedd8652f408f
sharonsohxuanhui@hotmail.com <sharonsohxuanhui@hotmail.com>
2025-01-15 17:31:11 +0800

    Edit the account page for resident

--- src/scripts/resident.js
+++ src/scripts/resident.js
@@ -39,19 +39,19 @@ function changeContent(page) {
 					Hello, here is your account summary!
 				</p>
 				<form>
-				<label for="name">Name:</label>
-				<input type="text" id="name" name="name"
-						placeholder="Your Name" required>
-				<label for="email">Email:</label>
-				<input type="email" id="email" name="email"
-						placeholder="Your Email" required>
-				<label for="message">Message:</label>
-				<textarea id="message" name="message"
-							placeholder="Your Message"
-							rows="4" required>
-					</textarea>
-				<button type="submit">Send Message</button>
-				</form>`;
+				<label for="name"><strong>Current Accepted Task</strong></label>
+                <div class="info-box">
+                    Clear level 4 trash
+                </div>
+                <label for="name"><strong>Voucher Balance</strong></label>
+                <div class="info-box">
+                    100 Points
+                </div>
+                <label for="name"><strong>Transaction History</strong></label>
+                <div class="info-box">
+                    21/1/2025    Pen    5 Points
+                </div>
+                </form>`;
 			break;
 
 		default:
